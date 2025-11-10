4bccb6219afe624ad17e1d00fa1f4ad1e21ab4f6
ze-isdeadbye <marques2006jose@gmail.com>
2025-11-10 11:26:37 +0000

    Versão com leitura de sensor da esquerda e da direita e envio do mapeamento para LEDs

--- ESP_CENTRAL/ESP_CENTRAL.ino
+++ ESP_CENTRAL/ESP_CENTRAL.ino
@@ -1,9 +1,33 @@
+#define motord 3
+#define motore 5
 void setup() {
   Serial.begin(9600);
-
+  pinMode(motore,OUTPUT);
+  pinMode(motord,OUTPUT);
 }
 
 void loop() {
-  delay(500);
-  Serial.println("ola !!!!");
+if (Serial.available()>0) {  
+String s = Serial.readStringUntil('\n');
+Serial.println(s);
+String distd=s.substring(10,15);
+double aux1=distd.toDouble();
+String diste=s.substring(16);
+double aux2=diste.toDouble();
+Serial.println(aux1);
+Serial.println(aux2);
+if(aux1<=2.00f){
+  aux1 =(aux1/2.00f)*(-255.0f)+255.0f;
+  analogWrite(motord,aux1);
+}else{
+  digitalWrite(motord,LOW);
+}
+if(aux2<=2.00f){
+  aux2 =(aux2/2.00f)*(-255.0f)+255.0f;
+  analogWrite(motore,aux2);
+}else{
+  digitalWrite(motore,LOW);
+}
+}
+
 }
--- ESP_LEITOR/ESP_LEITOR.ino
+++ ESP_LEITOR/ESP_LEITOR.ino
@@ -1,34 +1,54 @@
-#define trigPin 3 
-#define echoPin 2
+#define trigd 3 
+#define echod 2
+#define trige 5 
+#define echoe 4
 const long veloSom=344.0; //para temperatura de 20 graus 
 double dist=0.0;
 long unsigned int ultMed;
-double medeDist() { 
+double medeDistd() { 
   double distance;
   long duration; 
   //Send pulse 
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigd, LOW); 
   delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH); 
+  digitalWrite(trigd, HIGH); 
   delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigd, LOW); 
   //Wait for echo and measure time until it happens
-  duration=pulseIn(echoPin,HIGH);
+  duration=pulseIn(echod,HIGH);
+  //Compute distance 
+  distance = ((double)duration*veloSom)/(2000000.0f); 
+  return distance;
+}
+double medeDiste() { 
+  double distance;
+  long duration; 
+  //Send pulse 
+  digitalWrite(trige, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(trige, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(trige, LOW); 
+  //Wait for echo and measure time until it happens
+  duration=pulseIn(echoe,HIGH);
   //Compute distance 
   distance = ((double)duration*veloSom)/(2000000.0f); 
   return distance;
 }
 void setup() {
   Serial.begin (9600); 
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT);
+  pinMode(trigd, OUTPUT); 
+  pinMode(echod, INPUT);
+  pinMode(trige, OUTPUT); 
+  pinMode(echoe, INPUT);
   ultMed=millis();
 }
 
 void loop() {
   if(millis()-ultMed>=300){
-  double dist=medeDist();
-  Serial.print("distancia: ");Serial.println(dist);
+  double distd=medeDistd();
+  double diste=medeDiste();
+  Serial.print("distancia: ");Serial.print(distd);Serial.print(", ");Serial.println(diste);
   ultMed=millis();
 }
 }
