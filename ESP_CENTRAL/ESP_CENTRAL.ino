ba947bbd083116273b380bac301d16b08c181df6
ze-isdeadbye <marques2006jose@gmail.com>
2025-11-14 15:25:57 +0000

    Versão a considerar distância da frente

--- ESP_CENTRAL/ESP_CENTRAL.ino
+++ ESP_CENTRAL/ESP_CENTRAL.ino
@@ -1,5 +1,11 @@
 #define motord 3
 #define motore 5
+
+unsigned long proximoPulsoD = 0;
+unsigned long proximoPulsoE = 0;
+bool estadoMotorD = false;
+bool estadoMotorE = false;
+
 void setup() {
   Serial.begin(9600);
   pinMode(motore,OUTPUT);
@@ -7,27 +13,59 @@ void setup() {
 }
 
 void loop() {
+  double aux1=5.00,aux2=5.00,aux3=5.00;
 if (Serial.available()>0) {  
 String s = Serial.readStringUntil('\n');
 Serial.println(s);
 String distd=s.substring(10,15);
-double aux1=distd.toDouble();
-String diste=s.substring(16);
-double aux2=diste.toDouble();
+aux1=distd.toDouble();
+String diste=s.substring(16,21);
+aux2=diste.toDouble();
+String distf = s.substring(22);
+aux3= distf.toDouble();
 Serial.println(aux1);
 Serial.println(aux2);
-if(aux1<=2.00f){
-  aux1 =(aux1/2.00f)*(-255.0f)+255.0f;
-  analogWrite(motord,aux1);
-}else{
-  digitalWrite(motord,LOW);
+Serial.println(aux3);
+if(aux1<=1.00f){
+  proximoPulsoD=(aux1)*450+50;
+  if(aux3<=1.00f){
+    proximoPulsoD=max(proximoPulsoD*(aux3),50);
+  }
+} 
+if(aux2<=1.00f){
+  proximoPulsoE=(aux2)*450+50;
+  if(aux3<=1.00f){
+    proximoPulsoE=max(proximoPulsoE*(aux3),50);
+  }
 }
-if(aux2<=2.00f){
-  aux2 =(aux2/2.00f)*(-255.0f)+255.0f;
-  analogWrite(motore,aux2);
-}else{
-  digitalWrite(motore,LOW);
-}
+if (aux1>1.00f&&aux2>1.00f&&aux3<=1.00f){
+  proximoPulsoE=(aux3)*450+50;
+  proximoPulsoD=(aux3)*450+50;
 }
 
+}
+static unsigned long tD = 0;
+  if (aux1 <= 1.00f||aux3<=1.00f) {
+    if (millis() - tD >= proximoPulsoD) {
+      estadoMotorD = !estadoMotorD;
+      digitalWrite(motord, estadoMotorD);
+      tD = millis();
+    }
+  } else {
+    digitalWrite(motord, LOW); // desligar se > 1m
+    estadoMotorD = false;
+  }
+
+  // vibração do motor esquerdo
+  static unsigned long tE = 0;
+  if (aux2 <= 1.00f||aux3<=1.00f) {
+    if (millis() - tE >= proximoPulsoE) {
+      estadoMotorE = !estadoMotorE;
+      digitalWrite(motore, estadoMotorE);
+      tE = millis();
+    }
+  } else {
+    digitalWrite(motore, LOW); // desligar se > 1m
+    estadoMotorE = false;
+  }
 }
--- ESP_LEITOR/ESP_LEITOR.ino
+++ ESP_LEITOR/ESP_LEITOR.ino
@@ -2,53 +2,46 @@
 #define echod 2
 #define trige 5 
 #define echoe 4
+#define trigf 7
+#define echof 6
 const long veloSom=344.0; //para temperatura de 20 graus 
 double dist=0.0;
 long unsigned int ultMed;
-double medeDistd() { 
+
+double medeDist(int trig, int echo) { 
   double distance;
   long duration; 
-  //Send pulse 
-  digitalWrite(trigd, LOW); 
+   
+  digitalWrite(trig, LOW); 
   delayMicroseconds(2); 
-  digitalWrite(trigd, HIGH); 
+  digitalWrite(trig, HIGH); 
   delayMicroseconds(10); 
-  digitalWrite(trigd, LOW); 
-  //Wait for echo and measure time until it happens
-  duration=pulseIn(echod,HIGH);
-  //Compute distance 
+  digitalWrite(trig, LOW); 
+  
+  duration=pulseIn(echo,HIGH,30000);// timeout 30 ms (~5m)
+  if (duration == 0) return 5.0; // sem eco, é mais de 5m então assume 5m
+
   distance = ((double)duration*veloSom)/(2000000.0f); 
   return distance;
 }
-double medeDiste() { 
-  double distance;
-  long duration; 
-  //Send pulse 
-  digitalWrite(trige, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(trige, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(trige, LOW); 
-  //Wait for echo and measure time until it happens
-  duration=pulseIn(echoe,HIGH);
-  //Compute distance 
-  distance = ((double)duration*veloSom)/(2000000.0f); 
-  return distance;
-}
+
 void setup() {
   Serial.begin (9600); 
   pinMode(trigd, OUTPUT); 
   pinMode(echod, INPUT);
   pinMode(trige, OUTPUT); 
   pinMode(echoe, INPUT);
+  pinMode(trigf, OUTPUT); 
+  pinMode(echof, INPUT);
   ultMed=millis();
 }
 
 void loop() {
   if(millis()-ultMed>=300){
-  double distd=medeDistd();
-  double diste=medeDiste();
-  Serial.print("distancia: ");Serial.print(distd);Serial.print(", ");Serial.println(diste);
+  double distd=medeDist(trigd,echod);
+  double diste=medeDist(trige,echoe);
+  double distf=medeDist(trigf,echof);
+  Serial.print("distancia: ");Serial.print(distd);Serial.print(", ");Serial.print(diste);Serial.print(", ");Serial.println(distf);
   ultMed=millis();
 }
 }
