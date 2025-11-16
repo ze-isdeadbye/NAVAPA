d71df186944910608fc8aac23b04607989900566
ze-isdeadbye <marques2006jose@gmail.com>
2025-11-16 18:03:19 +0000

    Código do esp leitor alterado para utilizar o Giroscópio: BMI160

--- ESP_LEITOR/ESP_LEITOR.ino
+++ ESP_LEITOR/ESP_LEITOR.ino
@@ -1,9 +1,22 @@
+#include <Wire.h>
+#include <BMI160Gen.h>
 #define trigd 3 
 #define echod 2
 #define trige 5 
 #define echoe 4
 #define trigf 7
 #define echof 6
+
+const int BMI160_I2C_ADDRESS = 0x68;
+#define INTERRUPT_PIN 0 
+unsigned long int ultimamed;
+
+#define RESET_BUTTON_PIN 10   
+
+void stepInterrupt() {
+  
+}
+
 const long veloSom=344.0; //para temperatura de 20 graus 
 double dist=0.0;
 long unsigned int ultMed;
@@ -26,23 +39,60 @@ double medeDist(int trig, int echo) {
 }
 
 void setup() {
-  Serial.begin (9600);
-  Serial1.begin(9600,SERIAL_8N1,10,9); 
+  Serial1.begin(9600,SERIAL_8N1,21,20); 
+  Serial.begin(9600);
+  while (!Serial) { } 
+
+  pinMode(RESET_BUTTON_PIN, INPUT_PULLDOWN);
   pinMode(trigd, OUTPUT); 
   pinMode(echod, INPUT);
   pinMode(trige, OUTPUT); 
   pinMode(echoe, INPUT);
   pinMode(trigf, OUTPUT); 
   pinMode(echof, INPUT);
+
+  if (!BMI160.begin(BMI160GenClass::I2C_MODE, BMI160_I2C_ADDRESS, INTERRUPT_PIN)) {
+    Serial.println("BMI160 initialization failed!");
+    while (1);
+  }
+  BMI160.attachInterrupt(stepInterrupt);
+
+  BMI160.autoCalibrateGyroOffset();
+  BMI160.autoCalibrateXAccelOffset(0); // X-axis to 0g
+  BMI160.autoCalibrateYAccelOffset(0); // Y-axis to 0g
+  BMI160.autoCalibrateZAccelOffset(1); // Z-axis to +1g (gravity)
+  
+  BMI160.setFullScaleAccelRange(BMI160_ACCEL_RANGE_2G);
+  BMI160.setIntFreefallEnabled(true);
+
+  BMI160.setStepDetectionMode(BMI160_STEP_MODE_NORMAL);
+  BMI160.setStepCountEnabled(true);
+  BMI160.resetStepCount();
+  
+  delay(1000);
   ultMed=millis();
 }
 
 void loop() {
+  if(BMI160.getIntFreefallStatus()){
+    Serial.print("foi detetada uma queda!!!!!!!!!!");
+    Serial1.println("QUEDA");
+    while(1){};
+  }
+  if(digitalRead(RESET_BUTTON_PIN) == HIGH) {
+    BMI160.resetStepCount();
+    Serial.println("Counter reset!");
+    delay(300); // simple debounce delay
+  }
   if(millis()-ultMed>=300){
+  uint16_t currentSteps = BMI160.getStepCount();
+  int16_t rawTemp=BMI160.readTemperature();
+  float tempC = 23.0 + ((float)rawTemp / 512.0); 
   double distd=medeDist(trigd,echod);
   double diste=medeDist(trige,echoe);
   double distf=medeDist(trigf,echof);
-  Serial1.print("distancia: ");Serial1.print(distd);Serial1.print(", ");Serial1.print(diste);Serial1.print(", ");Serial1.println(distf);
+  Serial.print("distancia: ");Serial.print(distd);Serial.print(", ");Serial.print(diste);Serial.print(", ");Serial.print(distf);Serial.print(", ");Serial.print(currentSteps);Serial.print(", ");Serial.println(tempC);
+  Serial1.print("distancia: ");Serial1.print(distd);Serial1.print(", ");Serial1.print(diste);Serial1.print(", ");Serial1.print(distf);Serial1.print(", ");Serial1.print(currentSteps);Serial1.print(", ");Serial1.println(tempC);
   ultMed=millis();
 }
 }
